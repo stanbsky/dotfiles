5a5864dec1dc69b73b5e034dd51faf022f63cc3a
Stan Borzhemsky <stanbsky@protonmail.com>
2022-07-07 00:12:18 +0100

    Fix up bootstrap script

--- .github/bootstrap.sh
+++ .github/bootstrap.sh
@@ -1,5 +1,5 @@
 # Run with:
-# OUT="$(mktemp)"; wget -q -O - https://pathtofile > $OUT; . $OUT
+# OUT="$(mktemp)"; wget -q -O - https://raw.githubusercontent.com/stanbsky/dotfiles/master/.github/bootstrap.sh > $OUT; . $OUT
 
 DOTFILES="$HOME/.dotfiles"
 
@@ -10,7 +10,7 @@ dtf () {
 dtfnew () {
   git clone --bare $1 $DOTFILES
   dtf config --local status.showUntrackedFiles no
-  dtf switch -c base
+  dtf switch -c master
 
   echo "Please add and commit additional files"
   echo "using 'dtf add' and 'dtf commit', then run"
@@ -18,7 +18,7 @@ dtfnew () {
 }
 
 dtfrestore () {
-  git clone -b base --bare $1 $DOTFILES
+  git clone -b master --bare $1 $DOTFILES
   dtf config --local status.showUntrackedFiles no
   dtf checkout || echo -e 'Deal with conflicting files, then run (possibly with -f flag if you are OK with overwriting)\ndtf checkout'
 }
